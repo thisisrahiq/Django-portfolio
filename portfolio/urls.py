592891359566f24470d9ef3d73c455113d148eea
Rahiq-Mac <rahiq-mac@Rahiq-Macs-Mac-mini.local>
2026-04-26 17:11:47 +0600

    Change sitemap content-type to text/xml for better compatibility

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     path('admin-panel/login/', views.admin_login, name='admin_login'),
     path('admin-panel/logout/', views.admin_logout, name='admin_logout'),
     path('robots.txt', TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
-    path('sitemap.xml', TemplateView.as_view(template_name="sitemap.xml", content_type="application/xml")),
+    path('sitemap.xml', TemplateView.as_view(template_name="sitemap.xml", content_type="text/xml")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
